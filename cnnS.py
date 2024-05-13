33e4b0f2bb32f082525acd80536fe7c3519282c1
ronin-11 <94402374+ronin-11@users.noreply.github.com>
2024-05-13 20:53:50 +0200

    Update cnnS.py

--- cnnS.py
+++ cnnS.py
@@ -4,10 +4,7 @@ import csv
 
 def fetch_page(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+    return response.text if response.status_code == 200 else None
 
 def parse_homepage(url):
     html_content = fetch_page(url)
@@ -15,10 +12,9 @@ def parse_homepage(url):
         soup = BeautifulSoup(html_content, 'html.parser')
         all_news_divs = soup.select("div.container_lead-plus-headlines-with-images__item--type-section")
         for div in all_news_divs:
-            # Select all 'a' tags within the div and try to get the second one
             links = div.find_all('a')
-            if len(links) > 1:  # Ensure there is at least two links
-                news = links[1]  # Get the second link
+            if len(links) > 1:
+                news = links[1]
                 title_tag = news.find('span', class_='container__headline-text')
                 title = title_tag.get_text(strip=True) if title_tag else None
                 relative_link = news.get('href')
@@ -33,18 +29,28 @@ def parse_article(article_url):
         content = ' '.join([p.get_text(strip=True) for p in soup.select('div.article__content p.paragraph.inline-placeholder.vossi-paragraph-primary-core-light')])
         return content
 
+def save_to_csv(file_path, data):
+    with open(file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Category', 'Title', 'Content', 'Link', 'Source'])  # Writing the headers of CSV
+        for news_item in data:
+            writer.writerow([news_item['category'], news_item['title'], news_item['content'], news_item['link'], 'CNN'])
+
 def main():
     url = 'https://edition.cnn.com/sport'
     category = url.split('/')[-1]
 
-    with open('cnn_sports.csv', 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Category', 'Content', 'Link', 'Source', 'Title'])  # Writing the headers of CSV
+    news_data = []
+    for news_item in parse_homepage(url):
+        article_content = parse_article(news_item['link'])
+        news_data.append({
+            'category': category,
+            'title': news_item['title'],
+            'content': article_content,
+            'link': news_item['link']
+        })
 
-        for news_item in parse_homepage(url):
-            article_content = parse_article(news_item['link'])
-            # Write the data in the specified order
-            writer.writerow([category, article_content, news_item['link'], 'CNN', news_item['title']])
+    save_to_csv('cnn_sports.csv', news_data)
 
 if __name__ == "__main__":
     main()
