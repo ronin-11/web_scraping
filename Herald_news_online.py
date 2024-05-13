0f40bd68b8295c146287c842f8677d521b57c7dc
ronin-11 <94402374+ronin-11@users.noreply.github.com>
2024-05-13 20:33:49 +0200

    Update Herald_news_online.py

--- Herald_news_online.py
+++ Herald_news_online.py
@@ -4,10 +4,7 @@ import csv
 
 def fetch_page(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+    return response.text if response.status_code == 200 else None
 
 def parse_homepage(url):
     html_content = fetch_page(url)
@@ -25,18 +22,20 @@ def fetch_article_content(article_url):
         content_section = soup.select_one('div.post--content')
         return content_section.get_text(strip=True) if content_section else "No content found"
 
+def save_to_csv(file_path, data):
+    with open(file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Category', 'Content', 'Link', 'Source', 'Title'])  # Writing the headers of the CSV file
+        for article in data:
+            writer.writerow([article['title'], article['link'], article['content']])
+
 def main():
     url = 'https://www.herald.co.zw/category/articles/top-stories/'
     articles_info = parse_homepage(url)
+    for article in articles_info:
+        article['content'] = fetch_article_content(article['link'])
 
-    with open('herald_news_articles.csv', 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Category','Content','Link','Source','Title'])  # Writing the headers of the CSV file
-
-        for article in articles_info:
-            content = fetch_article_content(article['link'])
-            # Write the data in the specified order
-            writer.writerow([article['title'], article['link'], content])
+    save_to_csv('herald_news_articles.csv', articles_info)
 
 if __name__ == "__main__":
     main()
